c139cdce0d9f4d65427316e8b3893a96754739ac
Kirill Satalkin <baboch@mail.ru>
2024-02-26 21:42:13 +0300

    commit one

---
+++ CHeckAPP/TableChecksController.swift
@@ -0,0 +1,12 @@
+//
+//  TableChecksController.swift
+//  CHeckAPP
+//
+//  Created by Кирилл Саталкин on 26.02.2024.
+//
+
+import UIKit
+
+class TableChecksController: UITableViewController {
+    
+}
